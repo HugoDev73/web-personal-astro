3474d50376a759d2966a4007d4338ed0e20e9a4d
hugo <hugo.dev73@gmail.com>
2025-06-11 21:03:39 -0600

    navbar

---
+++ src/data/navigation.ts
@@ -0,0 +1,16 @@
+import type { NavLink } from "../types/NavLink";
+
+export const navigation: NavLink[] = [
+  {
+    path: "",
+    name: "Inicio",
+  },
+  {
+    path: "",
+    name: "Projectos",
+  },
+  {
+    path: "",
+    name: "Blog",
+  },
+];
